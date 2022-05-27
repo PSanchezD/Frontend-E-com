8bced3267f69a471ac86c77a0917bf837207b2fe
pedro sanchez <pedrosanchez707@gmail.com>
2022-05-26 21:58:35 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# the frontend
+building the frontend of my sticker e-com
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+
+
+function Navbar(){
+    return (
+    <>
+        <div className="nav-c">
+            <div className="logo">
+                Logo image
+            </div>
+            <div className="nav-link-c">
+                <a href="/">home</a>
+                <a href="/">About us</a>
+                <a href="/">Sticker</a>
+                <a href="/">contact us</a>
+            </div>
+        </div>
+    </>
+    )
+}
+export default Navbar
